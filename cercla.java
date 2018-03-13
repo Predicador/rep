f922b536371ce1f8d9a89996c4168f6c7a2ce073
Aitor Doblas Jordán <yodaitor@hotmail.com>
2018-03-13 16:16:12 +0100

    Create cercla.java

---
+++ cercla.java
@@ -0,0 +1,56 @@
+public class Cercle {
+    private float x;
+    private float y;
+    /**constructor buit*/
+    public Cercle(){
+        
+    }
+    
+      /**constructor per parametres*/
+   public Cercle(float x, float y){
+    this.x= x;
+    this.y=y;
+}
+
+/**constructor copiador*/
+    public Cercle(Cercle Cercle){
+        this.x= Cercle.x;
+        this.y= Cercle.y;
+    }
+
+    @Override
+    public String toString() {
+        return "Cercle{" + "x=" + x + ", y=" + y + '}';
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cercle other = (Cercle) obj;
+        if (Float.floatToIntBits(this.x) != Float.floatToIntBits(other.x)) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.y) != Float.floatToIntBits(other.y)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Float.floatToIntBits(this.x);
+        hash = 53 * hash + Float.floatToIntBits(this.y);
+        return hash;
+    }
+ }   
+    
